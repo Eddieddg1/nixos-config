679d2990781ba5b47627cdf96dfa10c87a340ea6
Eddieddg1 <lol.bender@outlook.com>
2026-04-15 16:52:52 +0200

    Message

--- virt.nix
+++ virt.nix
@@ -5,10 +5,12 @@
     libvirtd = {
       enable = true;
       qemu = {
+        vhostUserPackages = [ pkgs.virtiofsd ];
         swtpm.enable = true;
       };
     };
     spiceUSBRedirection.enable = true;
   };
   services.spice-vdagentd.enable = true;
+  services.spice-webdavd.enable = true;
 }
